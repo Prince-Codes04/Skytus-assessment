1beb4a7b14e2d884c9e05e2303fc132641925caf
Prince-Codes04 <your_github_princepatel10037@gmail.com>
2026-06-08 14:27:17 +0530

    Function to find factorial

---
+++ assessment-8/p3.py
@@ -0,0 +1,11 @@
+def factorial(num):
+    fact = 1
+
+    for i in range(1, num + 1):
+        fact *= i
+
+    return fact
+
+num = int(input("Enter a number: "))
+
+print("Factorial =", factorial(num))
