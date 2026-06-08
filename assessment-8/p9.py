6de2327c13183d3f85533aef50d658c4bf6d53df
Prince-Codes04 <your_github_princepatel10037@gmail.com>
2026-06-08 14:37:03 +0530

    Function to find area of rectangle

---
+++ assessment-8/p9.py
@@ -0,0 +1,7 @@
+def area_rectangle(length, width):
+    return length * width
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+print("Area of Rectangle =", area_rectangle(length, width))
