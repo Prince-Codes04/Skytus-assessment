43887f6904f134399daf7b96adc62eb58e00cd16
Prince-Codes04 <your_github_princepatel10037@gmail.com>
2026-06-28 15:43:52 +0530

    WeatherApp  Completed

---
+++ assessment-14/WeatherApp.py
@@ -0,0 +1,18 @@
+import requests
+
+city = input("Enter city name: ")
+
+api_key = "7accbaba78f9d817db571a3535b1fd6b"
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+if data["cod"] == 200:
+    print("City :", data["name"])
+    print("Temperature :", data["main"]["temp"], "°C")
+    print("Humidity :", data["main"]["humidity"], "%")
+    print("Weather :", data["weather"][0]["description"])
+else:
+    print("City not found.")
